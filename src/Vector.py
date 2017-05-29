0cf2c35b31e7ebe600e56d784aeffa38f5d69c7a
suushiemaniac <suushiemaniac@gmail.com>
2017-05-30 01:35:18 +0200

    Introduce testing suite methods

--- src/Vector.py
+++ src/Vector.py
@@ -2,29 +2,22 @@ import math
 
 
 class Vector:
-    def __init__(self, *components: int):
+    def __init__(self, *components: float):
         self.components = list(components)
 
     def scale(self, scalar: int) -> 'Vector':
-        self.components = map(lambda x: x * scalar, self.components)
+        self.components = list(map(lambda x: x * scalar, self.components))
         return self
 
     def add(self, other: 'Vector') -> 'Vector':
-        for i in range(0, len(self.components)):
-            self.components[i] = self.components[i] + other.components[i]
-
+        self.components = list(map(lambda z: z[0] + z[1], zip(self.components, other.components)))
         return self
 
     def sub(self, other: 'Vector') -> 'Vector':
         return self.add(other.scale(-1))
 
-    def dot(self, other: 'Vector') -> int:
-        dot = 0
-
-        for i in range(0, len(self.components)):
-            dot += self.components[i] * other.components[i]
-
-        return dot
+    def dot(self, other: 'Vector') -> float:
+        return sum(map(lambda z: z[0] + z[1], zip(self.components, other.components)))
 
     def norm(self) -> float:
         return math.sqrt(sum(map(lambda x: x ** 2, self.components)))
@@ -46,14 +39,12 @@ class Vector:
             self.scale(other)
             return self
 
+    def __str__(self):
+        return self.components.__str__()
+
     @staticmethod
     def span(one: 'Vector', two: 'Vector') -> 'Vector':
-        span = []
-
-        for i in range(0, len(one.components)):
-            span[i] = two.components[i] - one.components[i]
-
-        return Vector(*span)  # * is vararg unpacking
+        return Vector(*map(lambda z: z[0] - z[1], zip(two.components, one.components)))
 
     @staticmethod
     def distance(one: 'Vector', two: 'Vector') -> float:
--- src/analyze_embeddings.py
+++ src/analyze_embeddings.py
@@ -1,39 +1,72 @@
 #! /usr/bin/python3
+import re
 
-import math
 import sys
 
-def read_data(filename):
-	words = {}
-	with open(filename, "r") as file:
-		for line in file:
-			values = line.split()
-			if len(values) > 2:
-				words[values[0]] = tuple(map(float, values[1:]))
-	return words
+from src.Vector import Vector
 
-def get_distance(p, q):
-	sq_sum = 0 
-	for x,y in zip(p, q):
-		sq_sum += (x - y) ** 2
-	return math.sqrt(sq_sum)
 
-def analogy(words, a, b, c):
-	dist = get_distance(words[a], words[b])
-	distances = {}
-	candidates = []
-	for key in words.keys():
-		if key != c:
-			distances[key] = get_distance(words[c], words[key])
-	for key in distances.keys():
-		if abs(dist - distances[key]) <= 0.01:
-			candidates.append((key, distances[key]))
-	return sorted(candidates, key = lambda x: x[1])
+def read_data(filename: str) -> dict:
+    words = {}
+
+    with open(filename, "r") as file:
+        for line in file:
+            values = line.split()
+
+            if len(values) > 2:
+                words[values[0]] = Vector(*map(float, values[1:]))
+
+    return words
+
+
+def read_tests(filename: str) -> list:
+    cases = []
+
+    with open(filename, "r") as file:
+        for line in file:
+            values = re.findall('(.*):(.*) => (.*):(.*)', line)
+
+            if len(values) == 1:
+                cases.append(values[0])
+
+    return cases
+
+
+def analogy(words: dict, a: str, b: str, c: str) -> list:
+    check_dist = Vector.distance(words[a], words[b])
+    distances = {}
+    candidates = []
+
+    for key in words.keys():
+        if key != c:
+            distances[key] = Vector.distance(words[c], words[key])
+
+    for key in distances.keys():
+        if abs(check_dist - distances[key]) <= 0.01:
+            candidates.append((key, distances[key]))
+
+    return sorted(candidates, key=lambda x: x[1])
+
+
+def match_threshold(words: dict, a: str, b: str, c: str, d: str) -> float:
+    return Vector.distance(words[c].add(Vector.span(words[a], words[b])), words[d])
+
+
+def matches(words: dict, a: str, b: str, c: str, d: str) -> bool:
+    return match_threshold(words, a, b, c, d) <= 0.01
 
 
 if __name__ == "__main__":
-	files = sys.argv[1:]
-	assert len(files) == 2, "Kann nur zwei Dateien entgegennehmen!"
-	data0 = read_data(files[0])
-	data1 = read_data(files[1])
-	print(analogy(data1, "Fohlen", "Pferd", "Kalb"))
+    if len(sys.argv) != 2:
+        sys.stderr.write('Please specify a data input file and a test input file!')
+        exit(1)
+
+    if re.match('.*-brown-.*', sys.argv[0]):
+        sys.stderr.write('nope.')
+        exit(255)
+
+    tests = read_tests(sys.argv[0])
+    data = read_data(sys.argv[1])
+
+    for test in tests:
+        print(match_threshold(data, *test))
