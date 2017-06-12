9c903416c0093080b019e7778d08324d92e28028
suushiemaniac <suushiemaniac@gmail.com>
2017-06-13 01:22:41 +0200

    Update script for automated fetching

--- src/cache_embeddings.py
+++ src/cache_embeddings.py
@@ -1,12 +1,14 @@
 #! /usr/bin/python3
+import lzma
 import re
-import sys
+import requests
 
+from io import BytesIO
 from src.model.Vector import Vector
 from src.db.Database import Database
 
 
-def read_data(filename: str) -> dict:
+def read_file_data(filename: str) -> dict:
     words = {}
 
     with open(filename, "r") as file:
@@ -15,6 +17,18 @@ def read_data(filename: str) -> dict:
 
             if len(values) > 2:
                 words[values[0]] = Vector(*map(float, values[1:]))
+
+    return words
+
+
+def read_lzma_data(file: lzma.LZMAFile) -> dict:
+    words = {}
+
+    for line in file:
+        values = line.split()
+
+        if len(values) > 2:
+            words[values[0]] = Vector(*map(float, values[1:]))
 
     return words
 
@@ -28,19 +42,30 @@ def matches(words: dict, a: str, b: str, c: str, d: str) -> bool:
 
 
 if __name__ == "__main__":
-    file = "/home/suushie_maniac/Schreibtisch/German-cca-100-11.txt"
-
-    if re.match('.*-brown-.*', file):
-        sys.stderr.write('nope.')
-        exit(255)
-
-    data = read_data(file)
     db = Database('cl-cache', 'localhost', 'root', 'localsql')
 
-    db.begin_transaction()
+    url = "http://arne.chark.eu/emnlp2015/"
+    source = requests.get(url).text
 
-    for word, embedding in data.items():
-        print(word)
-        db.cache_word_embedding(word, embedding, 'cca', 'de')
+    for line in re.finditer(r"<li><a href=\"(http://chark\.synology\.me/arne/emnlp2015/(([A-Z][a-z]+?)-(.+?)-(\d+)-("
+                            r"\d+)\.txt\.xz))\">\2</a></li>", source):
+        link = line.group(1)
+        file = line.group(2)
 
-    db.commit()
+        lang = line.group(3)
+        corpus = line.group(4)
+        window = line.group(6)
+
+        if lang in ["German", "English", "French"] and corpus != "brown":
+            xz = requests.get(link, stream=True)
+
+            with lzma.LZMAFile(BytesIO(xz.content)) as f:
+                data = read_lzma_data(f)
+
+                db.begin_transaction()
+
+                for word, embedding in data.items():
+                    print(word)
+                    db.cache_word_embedding(word, embedding, corpus, lang)
+
+                db.commit()
